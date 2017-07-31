49f6e00c86eaa860a7718fc340660e22ef37e8d0
王亮亮 <1647855685@qq.com>
2017-07-31 11:55:42 +0800

    Create selectChange.js

---
+++ WebContent/webPage/js/selectChange.js
@@ -0,0 +1,8 @@
+$(".searchIndex select").on("click",function(){
+	$(".searchIndex select").css("background-color","#fff");
+	$(".searchIndex select").css("background-color","#fff");
+	
+})
+$(".searchIndex select").mouseover(function(){
+   $(".searchIndex select").css("background-color","#fff");
+});
